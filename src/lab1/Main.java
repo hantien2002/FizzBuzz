465c9cfe3c14200d1534021f35aec84eed0f7c5d
Tien Han <tien.han@mail.utoronto.ca>
2025-09-08 00:57:31 -0400

    task 3 changes

---
+++ src/lab1/Main.java
@@ -0,0 +1,12 @@
+package lab1;
+
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        FizzBuzz.doFizzBuzz(5);
+
+        String[] emptyArgs = {};
+        FizzBuzz.main(emptyArgs);
+    }
+}
